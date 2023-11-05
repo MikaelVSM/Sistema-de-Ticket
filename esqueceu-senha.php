33a79e790242707ccb3e15bd2964b894fc931719
Mikael Morais <mikaelvitorsm123@gmail.com>
2023-11-04 22:09:27 -0300

    Alteração no formulário e mudança da pagina de envio

--- esqueceu-senha.php
+++ esqueceu-senha.php
@@ -11,26 +11,34 @@
  <body>
             <div class="row">
                 <div class="offset-lg-4 col-lg-4 mt-5 p-5 card">
-                    <form action="?page=verificar-esqueceu-senha" method="POST">
+                    <form action="verificar-esqueceu-senha.php" method="POST">
                         <div class="mb-3">
-                            <label>Seu Usuário</label>
+                            <label>Informe seu Usuário</label>
                             <input type="text" name="nome_usuario" class="form-control">
                         </div>
                         <div class="mb-3">
-                            <label>Data de Nascimento</label>
-                            <input type="date" name="data_nascimento" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label>RGM</label>
-                            <input type="int" name="rgm" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label>3 Primeiros dígitos CPF</label>
-                            <input type="int" name="cpf" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <button type="submit">Acessar</button>
-                        </div>
+		                    <label>Escolha sua Pergunta de Segurança</label>
+		                        <select name="pergunta_seguranca_usuario" class="form-control">
+                                    <option>-= Escolha =-</option>
+                                    <option value="1">Qual o nome de seu tio favorito?</option>
+                                    <option value="2">Onde você encontrou seu/sua marido/mulher?</option>
+                                    <option value="3">Qual o apelido de seu primo mais velho?</option>
+                                    <option value="4">Qual o apelido de seu filho mais velho?</option>
+                                    <option value="5">Qual o apelido de seu filho mais novo? </option>
+                                    <option value="6">Qual o nome de sua sobrinha mais velha?</option>
+                                    <option value="7">Qual o nome de seu sobrinho mais velho?</option>
+                                    <option value="8">Qual o nome de sua tia favorita?</option>
+                                    <option value="9">Onde você passou sua lua-de-mel?</option>
+                                    <option value="10">Qual o nome de um professor importante para você?</option>
+		                        </select>
+	                     </div>
+                         <div class="mb-3">
+                            <label>Informe a Resposta</label>
+                            <input type="text" name="resposta_seguranca_usuario" class="form-control">
+                        </div>  
+                        <div class= "mb-3">
+                            <button type="submit">verificar</button>
+                        </div>  
                     </form>
                 </div>
             </div>
