33ac68e993b8e78d603f0b67a776bdf76f54bba8
Mikael Morais <mikaelvitorsm123@gmail.com>
2023-11-15 18:36:44 -0300

    Adicionado o controle de permissão para cada usuário

--- departamento-listar.php
+++ departamento-listar.php
@@ -1,6 +1,12 @@
-<h1>Listar Departamentos</h1>
-<a href="gerar_PDF_departamento.php">Gerar PDF</a>
+<h1>Listar Departamento</h1>
+
 <?php
+	// Obter os usuários da sessão
+	$tipo_usuario = $_SESSION['tipo_usuario'];
+
+	// Se o usuário for um administrador 
+	if($tipo_usuario == '1'){
+		echo '<a href="gerar_PDF_usuarios.php" style="color: black;">Gerar PDF</a>'; // Link amarelo
 	$sql = "SELECT * FROM departamento";
 
 	$res = $conn->query($sql);
@@ -17,8 +23,8 @@
 			print "<td>".$row->id_departamento."</td>";
 			print "<td>".$row->nome_departamento."</td>";
 			print "<td>
-					  <button onclick=\"location.href='?page=dashboard&pag=departamento-editar&id_departamento=".$row->id_departamento."';\" class='btn btn-success'>Editar</button>
-					  <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=dashboard&pag=departamento-salvar&acao=excluir&id_departamento=".$row->id_departamento."';}else{false;}\"  class='btn btn-danger'>Excluir</button>
+					  <button onclick=\"location.href='?page=dashboard&pag=departamento-editar&id_departamento=".$row->id_departamento."';\" class='btn btn-outline-success'>Editar</button>
+					  <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=dashboard&pag=departamento-salvar&acao=excluir&id_departamento=".$row->id_departamento."';}else{false;}\"  class='btn btn-outline-danger'>Excluir</button>
 			       </td>";
 			print "</tr>";
 		}
@@ -26,4 +32,27 @@
 	}else{
 		print "<p>Não encontrou resultados</p>";
 	}
+	
+}elseif($tipo_usuario != '1'){
+	$sql = "SELECT * FROM departamento";
+
+	$res = $conn->query($sql);
+
+	if($res->num_rows > 0){
+		print "<table class='table table-bordered table-striped table-hover'>";
+		print "<tr>";
+		print "<th>#</th>";
+		print "<th>Nome do Departamento</th>";
+		print "</tr>";
+		while($row = $res->fetch_object()){
+			print "<tr>";
+			print "<td>".$row->id_departamento."</td>";
+			print "<td>".$row->nome_departamento."</td>";
+			print "</tr>";
+		}
+		print "</table>";
+	}else{
+		print "<p>Não encontrou resultados</p>";
+	}
+}
 ?>
