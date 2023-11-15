8c744d82c6f42bfaa7378e1c2332a2f394a3dafc
Mikael Morais <mikaelvitorsm123@gmail.com>
2023-11-15 18:31:10 -0300

    Adicionado o controle de permissão para cada usuário

--- atendente-listar.php
+++ atendente-listar.php
@@ -1,6 +1,13 @@
 <h1>Listar Atendente</h1>
-<a href="gerar_PDF_atendente.php">Gerar PDF</a>
+
 <?php
+
+// Para obter os usuários da sessão
+$tipo_usuario = $_SESSION['tipo_usuario'];
+
+// Verificação se o usuário é um administrador
+if ($tipo_usuario == "1"){
+	echo '<a href="gerar_PDF_usuarios.php" style="color: black;">Gerar PDF</a>'; // Link amarelo
 	$sql = "SELECT * FROM usuario u
 			INNER JOIN usuario_departamento ud
 			ON u.id_usuario = ud.usuario_id_usuario
@@ -22,7 +29,7 @@
 			print "<td>".$row->nome_usuario."</td>";
 			print "<td>".$row->nome_departamento."</td>";
 			print "<td>
-					  <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=dashboard&pag=atendente-salvar&acao=excluir&id_usuario=".$row->id_usuario."';}else{false;}\"  class='btn btn-danger'>Excluir</button>
+					  <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=dashboard&pag=atendente-salvar&acao=excluir&id_usuario=".$row->id_usuario."';}else{false;}\"  class='btn btn-outline-danger'>Excluir</button>
 				   </td>";
 			print "</tr>";
 		}
@@ -30,4 +37,33 @@
 	}else{
 		print "<p>Não foram encontrados usuários</p>";
 	}
+	// Verificação para usuários diferentes de administradores
+}elseif($tipo_usuario != '1'){
+	$sql = "SELECT * FROM usuario u
+			INNER JOIN usuario_departamento ud
+			ON u.id_usuario = ud.usuario_id_usuario
+			INNER JOIN departamento d
+			ON d.id_departamento = ud.departamento_id_departamento
+			WHERE u.tipo_usuario = '2'";
+	$res = $conn->query($sql);
+	if($res->num_rows > 0){
+		print "<table class='table table-bordered table-striped table-hover'>";
+		print "<tr>";
+		print "<th>#</th>";
+		print "<th>Usuário</th>";
+		print "<th>Departamento</th>";
+		while ($row = $res->fetch_object()) {
+			print "<tr>";
+			print "<td>".$row->id_usuario."</td>";
+			print "<td>".$row->nome_usuario."</td>";
+			print "<td>".$row->nome_departamento."</td>";
+			print "</tr>";
+		}
+		print "</table>";
+	}else{
+		print "<p>Não foram encontrados usuários</p>";
+	}
+}
+
+	
 ?>
