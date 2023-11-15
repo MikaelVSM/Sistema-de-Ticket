f6fbfb680ff481619a03ce0063cf9e6f69da2628
Mikael Morais <mikaelvitorsm123@gmail.com>
2023-11-15 18:45:58 -0300

    Adicionado o controle de permissão para cada usuário

--- usuario-cadastrar.php
+++ usuario-cadastrar.php
@@ -1,4 +1,18 @@
+<?php
+
+	// Obtendo o tipo do usuário
+	$tipo_usuario = $_SESSION['tipo_usuario'];
+
+	// Verificando se é diferente de administrador
+	if($tipo_usuario != '1'){
+		//Alerta para usuários diferentes de usuários e caminho para outra página.
+		print "<script>alert('Você não tem permissão para acessar esta página');</script>";
+		print "<script>location.href='?page=dashboard';</script>";
+	}
+?>
+
 <h1>Cadastrar Usuário</h1>
+
 <form action="?page=dashboard&pag=usuario-salvar" method="POST">
 	<input type="hidden" name="acao" value="cadastrar">
 	<div class="mb-3">
